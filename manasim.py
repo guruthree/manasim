a455d9b31e57547b80a7e072474ad12aea8a3916
Fred <guru.three@gmail.com>
2018-02-21 22:23:26 +0000

    draw cards into hand

--- manasim.py
+++ manasim.py
@@ -16,15 +16,34 @@ with open(DATAFILE, 'r') as csvfile:
         i += 1
 
 library = np.array(deck)
+
 # shuffle the deck
 np.random.shuffle(library)
 #print(deck)
-print(library)
-print(len(library))
+#print(library)
+#print(len(library))
 
 hand = library[0:7]
 hand = np.sort(hand)
-print(hand)
+#print(hand)
 library = library[7:]
 print(library)
-print(len(library))
+#print(len(library))
+
+
+turn = 0
+numMana = 0
+
+# will eventually need to sort out if you start first or your opponent does
+while turn < 20:
+    # draw
+    hand = np.append(hand, library[0])
+    library = library[1:]
+    hand = np.sort(hand)
+
+
+
+    turn += 1
+
+print(hand)
+print(library)
